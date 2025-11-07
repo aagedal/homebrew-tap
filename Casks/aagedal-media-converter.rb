73623699fd71f1de6d8b145e47af1c9d8cf96214
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2025-11-07 21:54:24 +0100

    Fix URL formatting in aagedal-media-converter.rb

--- Casks/aagedal-media-converter.rb
+++ Casks/aagedal-media-converter.rb
@@ -2,7 +2,7 @@ cask "aagedal-media-converter" do
   version "2.6.5"
   sha256 "1919cbf404af10b1d1c9a403f3401d7b7f77a5de18bebdaf5528323cf180d66e"
 
-  url "https://github.com/aagedal/Aagedal-Media-Converter/releases/download/v.#{version}/AagedalMedia_Converter_#{version.tr(".", "-")}.zip",
+  url "https://github.com/aagedal/Aagedal-Media-Converter/releases/download/v.#{version}/Aagedal-Media-Converter_#{version.tr(".", "-")}.zip",
       verified: "github.com/aagedal/Aagedal-Media-Converter/"
   name "Aagedal Media Converter"
   desc "Minimalist FFMPEG front end written in SwiftUI, with support for batch conversion and watch folder."
