e8ab59d9b52eec1825670cf6ec9f254569a76ed7
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2025-11-20 12:51:28 +0100

    Update version and SHA256 for media converter

--- Casks/aagedal-media-converter.rb
+++ Casks/aagedal-media-converter.rb
@@ -1,6 +1,6 @@
 cask "aagedal-media-converter" do
-  version "2.9"
-  sha256 "4a31bbb1ff65b57ac7e3b6aa095c3bf10489fd17325f39a3c0c80ded759af7b0"
+  version "2.9.1"
+  sha256 "sha256:5c6143264b05b579077709e2fa2d40cf60117e52e9dc300ba9e46ca20942b142"
 
   url "https://github.com/aagedal/Aagedal-Media-Converter/releases/download/v.#{version}/Aagedal-Media-Converter_#{version.tr(".", "-")}.zip",
       verified: "github.com/aagedal/Aagedal-Media-Converter/"
