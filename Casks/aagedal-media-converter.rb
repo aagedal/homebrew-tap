32fb1eaf6539142e35457ba433e2f7c3fe25fb92
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2025-11-20 15:42:32 +0100

    Update version of Aagedal Media Converter to 2.9.2

--- Casks/aagedal-media-converter.rb
+++ Casks/aagedal-media-converter.rb
@@ -1,5 +1,5 @@
 cask "aagedal-media-converter" do
-  version "2.9.1"
+  version "2.9.2"
   sha256 "5c6143264b05b579077709e2fa2d40cf60117e52e9dc300ba9e46ca20942b142"
 
   url "https://github.com/aagedal/Aagedal-Media-Converter/releases/download/v.#{version}/Aagedal-Media-Converter_#{version.tr(".", "-")}.zip",
