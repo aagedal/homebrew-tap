3db2fbbd4783cfe822a9370be4464ed8e1c8fe84
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-01-27 19:49:14 +0100

    Fix URL format in aagedal-photo-agent.rb

--- Casks/aagedal-photo-agent.rb
+++ Casks/aagedal-photo-agent.rb
@@ -3,7 +3,7 @@ cask "aagedal-photo-agent" do
   version "1.0.0"
   sha256 "797656220b9f9e7017fc634c4476862e2a948fdbe7d687208d6b2a2c836aa853"
 
-  url "https://github.com/aagedal/Aagedal-Photo-Agent/releases/download/v.#{version}/Aagedal-Photo-Agent#{version.tr(".", "-")}.zip",
+  url "https://github.com/aagedal/Aagedal-Photo-Agent/releases/download/v.#{version}/Aagedal-Photo-Agent_#{version.tr(".", "-")}.zip",
       verified: "github.com/aagedal/Aagedal-Photo-Agent/"
   name "Aagedal Photo Agent"
   desc "Edit IPTC metadata of photos in batches with support for variables. AI face detection makes it faster to tag names of people."
