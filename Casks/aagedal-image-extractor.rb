a67c1ac267ebccdae96ed02150c9d825e823e011
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-02-12 21:25:18 +0100

    Fix URL for Aagedal Image Extractor download

--- Casks/aagedal-image-extractor.rb
+++ Casks/aagedal-image-extractor.rb
@@ -2,7 +2,7 @@ cask "aagedal-image-extractor" do
   version "1.1.0"
   sha256 "444b60b335b7b714ae877e16a0f034e4cf966493daf9e7ab1670e929179e47fb"
 
-  url "https://github.com/aagedal/Aagedal-Image-Extractor/tree/mainreleases/download/v.#{version}/Aagedal-Image-Extractor_#{version.tr(".", "-")}.zip",
+  url "https://github.com/aagedal/Aagedal-Image-Extractor/tree/releases/download/v.#{version}/Aagedal-Image-Extractor_#{version.tr(".", "-")}.zip",
       verified: "github.com/aagedal/Aagedal-Image-Extractor/"
   name "Aagedal Image Extractor"
   desc "Quickly batch extract images from PDFs and DOCX files with IPTC metadata."
