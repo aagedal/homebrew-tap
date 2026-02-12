aad2c1e6ddf85ac97b4a8e7951f9d9adfee23365
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-02-12 21:22:42 +0100

    Add Aagedal Image Extractor cask version 1.1.0

--- Casks/aagedal-image-extractor.rb
+++ Casks/aagedal-image-extractor.rb
@@ -1,1 +1,21 @@
+cask "aagedal-image-extractor" do
+  version "1.1.0"
+  sha256 "444b60b335b7b714ae877e16a0f034e4cf966493daf9e7ab1670e929179e47fb"
 
+  url "https://github.com/aagedal/Aagedal-Image-Extractor/tree/mainreleases/download/v.#{version}/Aagedal-Image-Extractor#{version.tr(".", "-")}.zip",
+      verified: "github.com/aagedal/Aagedal-Image-Extractor/"
+  name "Aagedal Image Extractor"
+  desc "Quickly batch extract images from PDFs and DOCX files with IPTC metadata."
+  homepage "https://github.com/aagedal/Aagedal-Image-Extractor/"
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :sequoia"
+
+  app "Aagedal Media Converter.app"
+
+  caveats do
+    <<~EOS
+      Thanks for installing Aagedal Image Extractor #{version}! I hope you like it :D
+    EOS
+  end
+end
