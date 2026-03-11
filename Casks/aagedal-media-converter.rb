d3fc53a4ec0d5aa88a7b900ac59036226d25c6c0
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-03-11 15:53:56 +0100

    Update URL format and remove caveats for cask

--- Casks/aagedal-media-converter.rb
+++ Casks/aagedal-media-converter.rb
@@ -2,7 +2,7 @@ cask "aagedal-media-converter" do
   version "3.7.3"
   sha256 "641d0a807c6db8147081776968d8306c59a786b0abad358b3c7263e72a729cc0"
 
-  url "https://github.com/aagedal/Aagedal-Media-Converter/releases/download/v.#{version}/Aagedal-Media-Converter#{version.tr(".", "-")}.zip",
+  url "https://github.com/aagedal/Aagedal-Media-Converter/releases/download/v.#{version}/Aagedal-Media-Converter_#{version.tr(".", "-")}.zip",
       verified: "github.com/aagedal/Aagedal-Media-Converter/"
   name "Aagedal Media Converter"
   desc "Minimalist FFMPEG front end written in SwiftUI, with support for batch conversion and watch folder."
@@ -12,10 +12,4 @@ cask "aagedal-media-converter" do
   depends_on macos: ">= :sequoia"
 
   app "Aagedal Media Converter.app"
-
-  caveats do
-    <<~EOS
-      Thanks for installing Aagedal Media Converter #{version}! I hope you like it :D
-    EOS
-  end
 end
