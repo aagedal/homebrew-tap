b245ccaf328f4e8bebeda38a425b7619a6d65bfb
taagedal <taagedal@noreply.codeberg.org>
2026-05-15 16:34:56 +0200

    Update Formula/swift-exif.rb

--- Formula/swift-exif.rb
+++ Formula/swift-exif.rb
@@ -1,7 +1,7 @@
 class SwiftExif < Formula
   desc "Native Swift media-metadata CLI for Exif, IPTC, XMP, and C2PA"
   homepage "https://codeberg.org/taagedal/SwiftExif"
-  version "1.6.0"
+  version "1.8.1"
   license "GPL-3.0-or-later"
 
   on_macos do
@@ -10,18 +10,7 @@ class SwiftExif < Formula
 
     on_arm do
       url "https://codeberg.org/taagedal/SwiftExif/releases/download/#{version}/swift-exif-macos-arm64"
-      sha256 "568df1d2c6ff3304d28ef0d91f0ca2bc44e6290f44a24a379ae1a37f024f0919"
-    end
-  end
-
-  on_linux do
-    on_intel do
-      url "https://codeberg.org/taagedal/SwiftExif/releases/download/#{version}/swift-exif-linux-x86_64"
-      sha256 "7ee27b16740abe716b20aba9386dcaae9e635ed639f2589a430948bf6885c213"
-    end
-    on_arm do
-      url "https://codeberg.org/taagedal/SwiftExif/releases/download/#{version}/swift-exif-linux-aarch64"
-      sha256 "e8c3fdbb41f6f76f761176a032cd56efb5fd3d2f2281bc56e1d8260e942d4d0b"
+      sha256 "4ce9cca1c36ad208496bfe1ddb237f2fa17a7913d728bb02d6e15c8f97d238a1"
     end
   end
 
