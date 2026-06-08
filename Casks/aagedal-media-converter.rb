2f07e824aeafcb907b72a2c50bf0bbe5ff6d9a5e
taagedal <taagedal@noreply.codeberg.org>
2026-06-08 10:05:17 +0200

    Update Casks/aagedal-media-converter.rb

--- Casks/aagedal-media-converter.rb
+++ Casks/aagedal-media-converter.rb
@@ -1,16 +1,16 @@
 cask "aagedal-media-converter" do
-  version "4.1.3"
-  sha256 "bc467e710a89fc1d15760dfb6d5e9b74c025ed126a5525754e47104544fd7f8c"
+  version "4.2.0"
+  sha256 "0a3f1a675e25c6dd34890b0c18b227b74599c6dcaed19718c8b6eb553658c186"
 
-  url "https://codeberg.org/taagedal/Aagedal-Media-Converter/releases/download/#{version}/Aagedal_Media_Converter_#{version.dots_to_hyphens}.zip",
-      verified: "codeberg.org/taagedal/Aagedal-Media-Converter/"
+  url "https://aagedal.me/apps/Aagedal_Media_Converter_#{version.dots_to_hyphens}.zip",
+      verified: "aagedal.me/apps/"
   name "Aagedal Media Converter"
   desc "Minimalist FFmpeg front end in SwiftUI with batch conversion and watch folders"
   homepage "https://codeberg.org/taagedal/Aagedal-Media-Converter"
 
   livecheck do
-    url "https://codeberg.org/taagedal/Aagedal-Media-Converter/releases.atom"
-    regex(%r{/tag/v?\.?(\d+(?:\.\d+)+)}i)
+    url "https://codeberg.org/taagedal/Aagedal-Media-Converter/raw/branch/main/appcast.xml"
+    regex(/sparkle:shortVersionString>(\d+(?:\.\d+)+)</i)
   end
 
   depends_on arch: :arm64
