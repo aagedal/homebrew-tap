30613af26b9fc4bd70628c926a6aa16e29ea0506
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-07-10 12:36:39 +0200

    Point cask to published DMG

--- Casks/aagedal-photo-agent.rb
+++ Casks/aagedal-photo-agent.rb
@@ -2,8 +2,8 @@ cask "aagedal-photo-agent" do
   version "2.1.0"
   sha256 "1ac2e10190c05ce259118c84809043e5a18e5a2521cc14a8318db047f0be7891"
 
-  url "https://codeberg.org/taagedal/Aagedal-Photo-Agent/releases/download/#{version}/Aagedal-Photo-Agent_#{version.tr(".", "-")}.zip",
-      verified: "codeberg.org/taagedal/Aagedal-Photo-Agent/"
+  url "https://aagedal.me/apps/Aagedal-Photo-Agent-#{version}.dmg",
+      verified: "aagedal.me/apps/"
   name "Aagedal Photo Agent"
   desc "Batch-edit IPTC photo metadata with variables and AI face-detection tagging"
   homepage "https://codeberg.org/taagedal/Aagedal-Photo-Agent"
