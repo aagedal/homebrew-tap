c5188813c527c904d7694a54a254f77b2efe5361
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-07-23 21:22:16 +0200

    Use GitHub Converter release

--- Casks/aagedal-media-converter.rb
+++ Casks/aagedal-media-converter.rb
@@ -2,8 +2,8 @@ cask "aagedal-media-converter" do
   version "4.3.0"
   sha256 "72bad6e3fdadb5434dcb3843d971cb7978849a74efdc4c09363a4ba0f7bce146"
 
-  url "https://aagedal.me/apps/Aagedal_Media_Converter_#{version.dots_to_hyphens}.zip",
-      verified: "aagedal.me/apps/"
+  url "https://github.com/aagedal/Aagedal-Media-Converter/releases/download/#{version}/Aagedal_Media_Converter_#{version.dots_to_hyphens}.zip",
+      verified: "github.com/aagedal/Aagedal-Media-Converter/"
   name "Aagedal Media Converter"
   desc "Minimalist FFmpeg front end in SwiftUI with batch conversion and watch folders"
   homepage "https://github.com/aagedal/Aagedal-Media-Converter"
